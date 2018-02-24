0ab63117d03538c714feb5d52cf33f3693daaf50
SuslikEst <cplusplusjs@gmail.com>
2018-02-24 20:24:46 +0300

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 var i = 0;
 var j = 0;
 
+var mas1 = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+var mas2 = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+
 function tim (){
 	var timer, timer2;
 	clearInterval(timer);
 	clearInterval(timer2);
 	timer =	setInterval(()=>{
+		
+		console.log(mas1[i]);
 		i++;
-		console.log(i);
+
 		if(i > 3){
 			clearInterval(timer);
 			i=0;
 			timer2 = setInterval(()=>{
-				j += 10;
-				console.log(j);
+				
+				console.log(mas2[j]);
 
-				if(j > 30){
+				j++;
+
+				if(j > 3){
 					clearInterval(timer2);
 					j=0;
 					tim();
