e3db24e5f1cfdb142c4cd49d64f08e6c924d77c7
cobweb <codename.cobweb@gmail.com>
2018-02-26 20:00:05 +0300

    fix

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 var i = 0;
 var j = 0;
 
-var mas1 = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
-var mas2 = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+var mas1 = ['images/group1/1.jpg', 'images/group1/2.jpg', 'images/group1/3.jpg', 'images/group1/4.jpg'];
+var mas2 = ['images/group2/1.jpg', 'images/group2/2.jpg', 'images/group2/3.jpg', 'images/group2/4.jpg'];
 
 function tim (){
 	var timer, timer2;
 	clearInterval(timer);
 	clearInterval(timer2);
-	timer =	setInterval(()=>{
-		
+	timer =	setInterval(function(){
+
+		$('#slide_show').fadeOut(600, function() {   
+		  $(this).attr('src', mas1[i]).fadeIn(600);
+		});
 		console.log(mas1[i]);
 		i++;
 
 		if(i > 3){
 			clearInterval(timer);
 			i=0;
-			timer2 = setInterval(()=>{
-				
+			timer2 = setInterval(function(){
+				$('#slide_show').fadeOut(600, function() {   
+				  $(this).attr('src', mas2[j]).fadeIn(600);
+				});
 				console.log(mas2[j]);
 
 				j++;
@@ -33,3 +38,21 @@ function tim (){
 }
 
 tim();
+
+// var n=0;
+// time=800;
+// play=setInterval("chgImg(0)", 5000);
+
+// function chgImg(number) {
+// if (number!=0) n=number-2;
+//  $('#slide_show').fadeOut(time, function() {   
+//   $(this).attr('src', imgs[n]).fadeIn(time);
+//  });
+
+//  $('#slide_text').fadeOut(time, function() {    //для текста
+//   $(this).html(text[n]).fadeIn(time);
+//  });
+
+// n++;
+// if (n>=imgs.length) n=0;
+// }
